5588c2850c082e5ee631851c70fa30c0533774e8
jruizndev <jorsqn @gmail.com>
2024-10-03 00:04:50 +0200

    Componentized chat, hero and navbar

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -1,64 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { MdSend } from 'react-icons/md'
+import React from 'react'
+import Nav from '../components/Nav'
+import ChatComponent from '../components/ChatComponent'
 
 const Chatbot = () => {
-    const [messages, setMessages] = useState([])
-    const [inputMessage, setInputMessage] = useState('')
-    const messagesEndRef = useRef(null)
-
-    const handleSendMessage = (e) => {
-        e.preventDefault()
-
-        if (inputMessage.trim() !== '') {
-            setMessages([...messages, { sender: 'user', text: inputMessage }])
-            setInputMessage('')
-        }
-    }
-
-    useEffect(() => {
-        if (messagesEndRef.current) {
-            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
-        }
-    }, [messages])
-
     return (
-        <div className="flex flex-col justify-between w-[80%] max-w-5xl mx-auto h-[80vh] bg-white rounded-lg m-5 px-5 font-outfit">
-            <div className="flex flex-col space-y-2 overflow-y-auto p-4 h-[70vh] bg-white rounded-lg border border-gray-300 shadow-md">
-                {messages.map((msg, index) => (
-                    <div
-                        key={index}
-                        className={`p-3 rounded-lg max-w-[80%] ${
-                            msg.sender === 'user'
-                                ? 'bg-blue-200 self-end text-right ml-auto mb-1'
-                                : 'bg-gray-200 self-start text-left mr-auto mb-1'
-                        } overflow-wrap break-words`}
-                    >
-                        {msg.text}
-                    </div>
-                ))}
-                <div ref={messagesEndRef} />
-            </div>
-            <form
-                onSubmit={handleSendMessage}
-                className="flex items-center rounded-lg border border-gray-300 p-2 bg-white rounded-b-lg shadow-md"
-            >
-                <input
-                    type="text"
-                    value={inputMessage}
-                    onChange={(e) => setInputMessage(e.target.value)}
-                    placeholder="Escribe tu mensaje"
-                    className="flex-grow px-4 py-2 outline-none rounded-lg w-4/5 font-outfit"
-                />
-                <button
-                    type="submit"
-                    className="bg-blue-200 p-2 rounded-full text-white shadow-lg transition-transform duration-150 ease-in-out transform active:scale-95"
-                >
-                    <div className="bg-blue-200 p-2 rounded-full flex items-center justify-center">
-                        <MdSend className="text-black" size={20} />
-                    </div>
-                </button>
-            </form>
-        </div>
+        <>
+            <Nav />
+            <section className="flex flex-col items-center justify-center min-h-screen">
+                <div className="relative flex w-full h-auto">
+                    <img
+                        src={require('../assets/images/ellipseMobileBlue.png')}
+                        alt="ellipseBlue"
+                        className="w-[80%] absolute left-0 top-0"
+                    />
+                    <img
+                        src={require('../assets/images/ellipseMobileGreen.png')}
+                        alt="ellipseGreen"
+                        className="w-[50%] absolute right-0 top-0"
+                    />
+                </div>
+                <div className="w-full mt-[7%] px-8 mb-[4%] text-center font-outfit text-lg md:text-xl lg:text-2xl p-4">
+                    {' '}
+                    <p className="p-4 text-l">
+                        ¿Listo para descubrir tu verdadera vocación y
+                        encaminarte hacia una carrera que te apasione? Nuestro
+                        chatbot inteligente te ayuda a explorar opciones
+                        profesionales personalizadas, basadas en tus intereses,
+                        habilidades y metas. Recibe orientación clara para tomar
+                        decisiones con confianza.
+                        <br />
+                        Da el primer paso hacia un futuro lleno de oportunidades
+                        y encuentra una carrera que te motive. ¡Comienza hoy
+                        mismo tu camino hacia el éxito profesional!
+                    </p>
+                </div>
+                <ChatComponent />
+            </section>
+        </>
     )
 }
 
