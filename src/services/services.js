76d18e251e92f4040c91aee31ad3aef53b072b85
jfigueira87 <jfigueira@gmail.com>
2024-10-03 13:05:34 +0200

    Adding validations and requests to validate the user

--- src/services/services.js
+++ src/services/services.js
@@ -1,15 +1,13 @@
-import axios from "axios"; 
+import axios from 'axios'
 
-
-export const URL = 'http://localhost:3000/nextstep'; 
-
+export const URL = 'http://localhost:3000/nextstep'
 
 export const postRegisterData = async (data) => {
-  try {
-      const response = await axios.post(URL, data);
-      return response.data
-  } catch (error) {
-      console.error('Error al registrarte:', error);
-      throw error;
-  }
+    try {
+        const response = await axios.post(URL, data)
+        return response.data
+    } catch (error) {
+        console.error('Error al registrarte:', error)
+        throw error
+    }
 }
