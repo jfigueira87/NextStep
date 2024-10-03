0c6474967a0cfeab9d9eb10e3d81b9f291029b6b
devegalaura-dev <lauradevega94@gmail.com>
2024-10-03 21:02:24 +0200

    POST is working

--- src/services/services.js
+++ src/services/services.js
@@ -1,18 +1,20 @@
 import axios from "axios"; 
 
 
-export const URL = 'http://localhost:3000/nextstep'; 
+export const URL = 'http://localhost:4000/registerUsers'; 
 
 
 export const postRegisterData = async (data) => {
   try {
-      const response = await axios.post(URL, data);
-      return response.data
+    const response = await axios.post(URL, data);
+    console.log('Respuesta del servidor:', response); // <-- Añade este log
+    return response.data;
   } catch (error) {
-      console.error('Error al registrarte:', error);
-      throw error;
+    console.error('Error al registrarte:', error.response); // <-- Muestra más detalles del error
+    throw error;
   }
-}
+};
+
 export async function getAllusers() {
   try {
     const response = await axios.get(URL);
