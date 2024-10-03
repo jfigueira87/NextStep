d34241120df0bcc2d306523b0b966b8055b27a65
jfigueira87 <jfigueira@gmail.com>
2024-10-03 22:04:25 +0200

    Add Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-function Button() {
+const Button = ({ children, onClick, className, type = 'button', style }) => {
   return (
-    <div>
-      
-    </div>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`flex justify-center rounded-md px-3 py-1.5 text-sm font-bold leading-6 ${className}`}
+      style={style}
+    >
+      {children}
+    </button>
   )
 }
 
