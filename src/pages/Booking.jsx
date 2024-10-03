7e3a44c1cfadaa6e04cecf94ad4becb5df0148eb
jruizndev <jorsqn@gmail.com>
2024-10-03 13:28:25 +0200

    changes in booking and add service emailjs

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Swal from 'sweetalert2'
 import Nav from '../components/Nav'
+import emailjs from 'emailjs-com'
 
 function Booking() {
     const [fullName, setFullName] = useState('')
     const [phoneNumber, setPhoneNumber] = useState('')
+    const [email, setEmail] = useState('')
     const [appointmentDate, setAppointmentDate] = useState('')
     const [appointmentTime, setAppointmentTime] = useState('')
     const [note, setNote] = useState('')
@@ -17,36 +19,42 @@ function Booking() {
         let validationErrors = {}
 
         if (!fullName) {
-            validationErrors.fullName = true
+            validationErrors.fullName =
+                'Debes completar este campo: Nombre y Apellidos.'
         } else if (!/^[A-Za-záéíóúÁÉÍÓÚÑñ\s]+$/.test(fullName)) {
             validationErrors.fullName =
                 'El nombre solo debe contener letras y espacios.'
         }
 
         if (!phoneNumber) {
-            validationErrors.phoneNumber = true
+            validationErrors.phoneNumber =
+                'Debes completar este campo: Número de teléfono.'
         } else if (!/^\d{9}$/.test(phoneNumber)) {
-            if (!/^\d+$/.test(phoneNumber)) {
-                Swal.fire({
-                    title: 'Error',
-                    text: 'Debes añadir un número de teléfono.',
-                    icon: 'error',
-                    confirmButtonText: 'Aceptar',
-                })
-            }
             validationErrors.phoneNumber =
                 'El número de teléfono debe tener exactamente 9 dígitos.'
         }
 
+        if (!email) {
+            validationErrors.email =
+                'Debes completar este campo: Correo electrónico.'
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            validationErrors.email =
+                'Debes introducir un correo electrónico válido.'
+        }
+
         if (!appointmentDate) {
-            validationErrors.appointmentDate = true
+            validationErrors.appointmentDate =
+                'Debes completar este campo: Fecha de la cita.'
         }
 
         if (!appointmentTime) {
-            validationErrors.appointmentTime = true
+            validationErrors.appointmentTime =
+                'Debes completar este campo: Hora de la cita.'
         }
 
         if (Object.keys(validationErrors).length === 0) {
+            sendEmails()
+
             Swal.fire({
                 title: 'Reserva con éxito!',
                 text: 'Tu cita ha sido reservada. Redirigiendo a la página del chatbot...',
@@ -60,43 +68,58 @@ function Booking() {
                 navigate('/chatbot')
             }, 2000)
         } else {
-            if (validationErrors.fullName) {
-                Swal.fire({
-                    title: 'Campo obligatorio',
-                    text: 'Debes completar este campo: Nombre y Apellidos.',
-                    icon: 'warning',
-                    confirmButtonText: 'Aceptar',
-                })
-            }
-
-            if (validationErrors.phoneNumber) {
-                Swal.fire({
-                    title: 'Campo obligatorio',
-                    text: 'Debes completar este campo: Número de teléfono.',
-                    icon: 'warning',
-                    confirmButtonText: 'Aceptar',
-                })
-            }
-
-            if (validationErrors.appointmentDate) {
-                Swal.fire({
-                    title: 'Campo obligatorio',
-                    text: 'Debes completar este campo: Fecha de la cita.',
-                    icon: 'warning',
-                })
-            }
-
-            if (validationErrors.appointmentTime) {
-                Swal.fire({
-                    title: 'Campo obligatorio',
-                    text: 'Debes completar este campo: Hora de la cita.',
-                    icon: 'warning',
-                    confirmButtonText: 'Aceptar',
-                })
-            }
-
             setErrors(validationErrors)
         }
+    }
+
+    const sendEmails = () => {
+        const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID
+        const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
+        const userId = process.env.REACT_APP_EMAILJS_USER_ID
+
+        const templateParams = {
+            fullName,
+            phoneNumber,
+            email,
+            appointmentDate,
+            appointmentTime,
+            note,
+            recipient_email: 'jorsqn@gmail.com',
+        }
+
+        emailjs
+            .send(serviceId, templateId, templateParams, userId)
+            .then((response) => {
+                console.log(
+                    'Email enviado con éxito',
+                    response.status,
+                    response.text
+                )
+            })
+            .catch((err) => {
+                console.error('Error al enviar el correo:', err)
+            })
+
+        emailjs
+            .send(
+                serviceId,
+                templateId,
+                {
+                    ...templateParams,
+                    recipient_email: email,
+                },
+                userId
+            )
+            .then((response) => {
+                console.log(
+                    'Correo de confirmación enviado con éxito',
+                    response.status,
+                    response.text
+                )
+            })
+            .catch((err) => {
+                console.error('Error al enviar el correo de confirmación:', err)
+            })
     }
 
     const minDate = new Date(2024, 9, 1).toISOString().split('T')[0]
@@ -186,6 +209,24 @@ function Booking() {
 
                         <div>
                             <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                placeholder="Correo electrónico"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                                className="block w-full rounded-md p-2 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                            {errors.email && (
+                                <p className="text-red-500 text-sm">
+                                    {errors.email}
+                                </p>
+                            )}
+                        </div>
+
+                        <div>
+                            <input
                                 id="appointment-date"
                                 name="appointment-date"
                                 type="date"
@@ -216,9 +257,7 @@ function Booking() {
                                 required
                                 className="block w-full rounded-md p-2 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             >
-                                <option value="" disabled>
-                                    Selecciona una hora
-                                </option>
+                                <option value="">Selecciona una hora</option>
                                 {generateTimeOptions()}
                             </select>
                             {errors.appointmentTime && (
@@ -232,23 +271,21 @@ function Booking() {
                             <textarea
                                 id="note"
                                 name="note"
-                                placeholder="Nota (opcional)"
+                                placeholder="Notas adicionales"
                                 value={note}
                                 onChange={(e) => setNote(e.target.value)}
                                 className="block w-full rounded-md p-2 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
 
-                        <button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-bold leading-6"
-                            style={{
-                                backgroundColor: '#B0EFFF',
-                                color: '#054D41',
-                            }}
-                        >
-                            Reservar Cita
-                        </button>
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Reservar cita
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
