3c0e29fd26863c4e8daf7968ce1403908f5cf6d4
Mª Rosa Cuenca <mariarosacuenca@gmail.com>
2024-10-03 23:09:21 +0200

    mensaje duplicado corregido

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router/Router'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <RouterProvider router={router}/>
-  </StrictMode>,
+  // </StrictMode>,
 )
