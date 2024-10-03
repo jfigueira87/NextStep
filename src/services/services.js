1c6d24b875f1a4a03c03ff18ef99caf9e5e52243
jfigueira87 <jfigueira@gmail.com>
2024-10-03 13:05:34 +0200

    Adding validations and requests to validate the user

--- src/services/services.js
+++ src/services/services.js
@@ -1,11 +1,10 @@
-//Relleno de prueba para que no esté vacío
 
 import axios from "axios";
 const URL_API = "http://localhost:8000/meme";
-const URL_STORAGE = "https://api.cloudinary.com/v1_1/dz53okn10/image/upload";
 
-//Get all memes -- GET
-export async function getAllMemes() {
+
+//Get all users -- GET
+export async function getAllusers() {
   try {
     const response = await axios.get(URL_API);
     return response.data;
